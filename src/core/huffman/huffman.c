5cea40882a93e87db32199c1e2101b61afc8acaf
piskounov <lamri.mohamed.yassine01@gmail.com>
2025-05-11 11:21:58 +0100

    Initially finished huffman segmenent

--- src/core/huffman/huffman.c
+++ src/core/huffman/huffman.c
@@ -73,4 +73,84 @@ void insertMinHeap(minheap* minHeap, minheapNode* minHeapNode){
   minHeap->array[i] = minHeapNode;
 }
 
+void buildMinHeap(minheap* minHeap){
+  int n = minHeap->size - 1;
+  int i;
 
+  for (i = (n-1)/2; i>=0; --i){
+    minHeapify(minHeap, i)
+  }
+}
+
+void printArr(int arr[], int n){
+  int i;
+  for (i=0; i<n; ++i){
+    printf("%d\n", arr[i]);
+  }
+}
+
+int isLeaf(minheapNode* root){
+  return !(root->left) && !(root->right);
+}
+
+minheap* createAndbuildMinheap(byte data[], int freq[], int size){
+  minheap* minHeap = createMinheap(size);
+  for (int i=0; i<size; ++i){
+    minHeap->array[i] = newNode(data[i], freq[i]);
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+    return minHeap;
+  }
+}
+
+minheapNode* buildHuffmanTree(byte data[], int freq[], int size){
+  minheapNode* left, right, top;
+  minheap* minHeap = createAndbuildMinheap(data, freq, size);
+
+  while (!isSizeOne(minHeap)){
+    left = extractMin(minHeap);
+    right = extractMin(minHeap);
+    top = newNode("$", left->freq + right->freq);
+    top->left = left;
+    top->right = right;
+
+    insertMinHeap(minHeap, top);
+  }
+  
+  return extractMin(minHeap);
+}
+
+void printCodes(minHeapNode* root, int arr[], int top){
+  if (root->left){
+    arr[top] = 0;
+    printCodes(root->left, arr, top+1);
+  }
+
+  if (root->right){
+    arr[top] = 1;
+    printCodes(root->right, arr, top+1);
+  }
+
+  if (isLeaf(root)){
+    printf("%c: " root->data);
+    printArr(arr, top);
+  }
+}
+
+void HuffmanCodes(byte data[], int freq[], int size){
+    minheapNode* root = buildHuffmanTree(data, freq, size);
+    int arr[MAX_TREE_HT], top = 0;
+    printCodes(root, arr, top);
+}
+
+int main(int argc, char **argv){
+
+    char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+    int freq[] = { 5, 9, 12, 13, 16, 45 };
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    HuffmanCodes(arr, freq, size);
+
+    return 0;
+}
